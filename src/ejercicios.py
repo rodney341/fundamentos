3cd550f22861a68ad885ce59b1e30b15114bca44
rodney <rodney072@misena.edu.co>
2022-06-16 12:19:47 -0500

    ejercicio semana 3

--- src/ejercicios.py
+++ src/ejercicios.py
@@ -3,5 +3,23 @@ Created on 14/06/2022
 
 @author: rodney
 '''
+numero = input("ingrese un numero")
+aux=0
+for x in numero:
+    aux=aux+1
+if aux == 1:
+    print("unidad")
+elif(aux==2):
+    print("decena")
+elif(aux==3):
+    print("centena")
+elif(aux==2):
+    print("milésima")
+elif(aux==2):
+    print("decena de mil")
+elif(aux==2):
+    print("centena de mil")
+
+
 print("hola mundo maravilloso")
 print("hola desde el computador portatil")
